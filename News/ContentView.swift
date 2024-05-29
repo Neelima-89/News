9da186bb3162c94f003be1da9ca1a6bb5f782d9f
Neelima-89 <neelimadarapureddy@gmail.com>
2024-05-29 10:00:06 +0530

    Initial Commit

---
+++ News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Neelima on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Neelima on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
